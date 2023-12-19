ddf0cee7f7d5bfc6960a9872fc27241f2e8cd17a
Tara <144671692+TaraG17@users.noreply.github.com>
2023-12-19 11:50:02 +0100

    Update Module_3.py

--- Module_3.py
+++ Module_3.py
@@ -6,20 +6,92 @@ from functools import partial
 
 #---Welcome message customizable per Station using button---
 def onclick(message):
+    global fac_frame
     welkom.config(text='Welcome to NS, Station ' + message)
     locatie = message #Depending on button for designated station
 
     #---Showing the weather for the specific station---
     api_key = "befce6c12cc4de8793e5f9de6f2ace3a"
-    #locatie = ""
     resource_uri = f"https://api.openweathermap.org/data/2.5/weather?q={locatie}&appid={api_key}"
     response = requests.get(resource_uri)
     response_data = response.json()
 
-    weather = response_data['weather'][0]['description'] #weer omschrijving
-    temp = response_data['main']['temp']-272.15 #temperatuur van kelvin naar ceclius
-
+    weather = response_data['weather'][0]['description'] #Weather description from API
+    temp = response_data['main']['temp']-272.15 #Calculates the temp from fahrenheit to celcius
     weer.config(text='The weather today has ' + weather + ' with a temperature of ' + str(round(temp)) + ' degrees.')
+
+    # ---Connection with the Database for facilities---
+    connection_string = "host='4.234.116.133' dbname='stationszuil' user='postgres' password='geheimpje1234'"
+    conn = psycopg2.connect(connection_string)
+    cursor = conn.cursor()
+    query_station = """ SELECT ov_bike, elevator, toilet, park_and_ride 
+                           FROM station_service 
+                           WHERE station_city = %s """
+    cursor.execute(query_station, (locatie,)) #Does the facilities per station
+    faciliteiten = cursor.fetchone()  #Grabs the row 
+    conn.close()
+
+#---If Frame exists, then destroys---
+    if fac_frame:
+        fac_frame.destroy()
+
+#---Frame for the facilities---
+    fac_frame = Frame(master=root, background='gold') #Seperate frame so that the labels don't get doubled 
+    fac_frame.pack()
+
+    # ---Header for facilities---
+    faciliteitenlabel = Label(master=fac_frame,
+                              text='Facilities:',
+                              background='white',
+                              foreground='dark blue',
+                              font=('Sans', 16, 'bold'),
+                              width=40,
+                              height=2)
+    faciliteitenlabel.pack(pady=10)
+
+    # ---Facilities at designated station---
+    bike, elevator, toilet, parkride = faciliteiten
+    if faciliteiten:
+        # ---Bikes---
+        if bike:
+            fietsenlabel = Label(master=fac_frame,
+                                 text='There are bikes available at this station',
+                                 background='white',
+                                 foreground='dark blue',
+                                 font=('Sans', 16, 'bold'),
+                                 width=40,
+                                 height=2)
+            fietsenlabel.pack(pady=2)
+        # ---Elevators---
+        if elevator:
+            liftenlabel = Label(master=fac_frame,
+                                text='There are elevators available at this station',
+                                background='white',
+                                foreground='dark blue',
+                                font=('Sans', 16, 'bold'),
+                                width=40,
+                                height=2)
+            liftenlabel.pack(pady=1)
+        # ---Toilets---
+        if toilet:
+            toilettenlabel = Label(master=fac_frame,
+                                   text='There are toilets available at this station',
+                                   background='white',
+                                   foreground='dark blue',
+                                   font=('Sans', 16, 'bold'),
+                                   width=40,
+                                   height=2)
+            toilettenlabel.pack(pady=1)
+        # ---Park and Ride---
+        if parkride:
+            parkridelabel = Label(master=fac_frame,
+                                  text='There is P+R available at this station',
+                                  background='white',
+                                  foreground='dark blue',
+                                  font=('Sans', 16, 'bold'),
+                                  width=40,
+                                  height=2)
+            parkridelabel.pack(pady=1)
 
 #---Page Setup---
 root = Tk()
@@ -41,7 +113,7 @@ button_style = {
     "background": "white", #Colour of button
     "foreground": "darkblue", #Colour of font
     "activebackground": "grey", #Colour when pressed
-    "activeforeground": "darkblue",
+    "activeforeground": "darkblue", #Colour of font when pressed 
     "font": ('Sans', 10, 'bold'), #Font style
     "relief": "raised", #Style of button
     "pady": 2} #Thickness
@@ -85,7 +157,7 @@ cursor = conn.cursor()	# DictCursor, not the default cursor!
 query_berichten = """SELECT bericht, station 
            FROM bericht 
            INNER JOIN keuring ON keuring.berichtid=bericht.berichtid 
-           WHERE beoordeling = 'Ja' ORDER BY bericht.tijd DESC LIMIT 5;"""
+           WHERE beoordeling = 'Ja' ORDER BY bericht.tijd DESC LIMIT 5;""" #Limiting the last 5 messages
 cursor.execute(query_berichten)
 records = cursor.fetchall()
 conn.close()
@@ -100,126 +172,10 @@ for record in records:
                              width=43,
                              height=2)
     showinglabel.pack(pady=1)
-        # showinglabel.place(x=75, y=140) anchor='w'
     station = record[1]
 
-    connection_string = "host='4.234.116.133' dbname='stationszuil' user='postgres' password='geheimpje1234'"
-    conn = psycopg2.connect(connection_string)
-    cursor = conn.cursor()
-    query_station = """ SELECT ov_bike, elevator, toilet, park_and_ride 
-                       FROM station_service 
-                       WHERE station_city = 'Utrecht' """
-    cursor.execute(query_station)
-    faciliteiten = cursor.fetchall()
-    conn.close()
-
-#---Header for facilities---
-faciliteitenlabel = Label(master=root,
-               text='Facilities:',
-               background='white',
-               foreground='dark blue',
-               font=('Sans', 16, 'bold'),
-               width=40,
-               height=2)
-faciliteitenlabel.pack(pady=10)
-
-#---Bikes at designated station---
-for fiets in faciliteiten:
-    ov_bike = f'{faciliteiten[0]}'
-    gesplitst = ov_bike.strip('()').split(',')
-    bike = gesplitst[0]
-    if bike == 'True':
-        fietsenlabel = Label(master=root,
-               text='There are bikes available at this station',
-               background='white',
-               foreground='dark blue',
-               font=('Sans', 16, 'bold'),
-               width=40,
-               height=2)
-    else:
-        fietsenlabel = Label(master=root,
-                             text='There are no bikes available at this station',
-                             background='white',
-                             foreground='dark blue',
-                             font=('Sans', 16, 'bold'),
-                             width=40,
-                             height=2)
-    fietsenlabel.pack(pady=2)
-#---Lifts at designated station---
-for lifts in faciliteiten:
-    elevator = f'{faciliteiten[0]}'
-    gesplitst = elevator.strip('()').split(',')
-    lift = gesplitst[1]
-    if lift == 'True':
-        liftenlabel = Label(master=root,
-               text='There are elevators available at this station',
-               background='white',
-               foreground='dark blue',
-               font=('Sans', 16, 'bold'),
-               width=40,
-               height=2)
-    else:
-        liftenlabel = Label(master=root,
-                             text='There are no elevators available at this station',
-                             background='white',
-                             foreground='dark blue',
-                             font=('Sans', 16, 'bold'),
-                             width=40,
-                             height=2)
-    liftenlabel.pack(pady=1)
-
-#---Toilets at designated station---
-for toilets in faciliteiten:
-    wc = f'{faciliteiten[0]}'
-    gesplitst = wc.strip('(').split(',')
-    toilet = gesplitst[2]
-    if toilet == ' True':
-        toilettenlabel = Label(master=root,
-               text='There are toilets available at this station',
-               background='white',
-               foreground='dark blue',
-               font=('Sans', 16, 'bold'),
-               width=40,
-               height=2)
-    else:
-        toilettenlabel = Label(master=root,
-                             text='There are no toilets available at this station',
-                             background='white',
-                             foreground='dark blue',
-                             font=('Sans', 16, 'bold'),
-                             width=40,
-                             height=2)
-    toilettenlabel.pack(pady=1)
-
-#---Park&Ride at designated station---
-for parking in faciliteiten:
-    pr = f'{faciliteiten[0]}'
-    gesplitst = pr.strip('(').split(',')
-    park = gesplitst[2]
-    if park == ' True':
-        parkridelabel = Label(master=root,
-               text='There is P+R available at this station',
-               background='white',
-               foreground='dark blue',
-               font=('Sans', 16, 'bold'),
-               width=40,
-               height=2)
-    else:
-        parkridelabel = Label(master=root,
-                             text='There is no P+R available at this station',
-                             background='white',
-                             foreground='dark blue',
-                             font=('Sans', 16, 'bold'),
-                             width=40,
-                             height=2)
-    parkridelabel.pack(pady=1)
-
-#---Facility Photos (I gave up)---
-#img = PhotoImage(file='images/img_lift.png')
-#foto = Label(master=root,
-             # image=img)
-#foto.pack()
-#foto.lift()
+#---Frame for Facilities---
+fac_frame = Frame(master=root, background='gold')
 
 #---That the root remains running---
 root.mainloop()
