ee620b341714c2e6a208278fffb9345ef557a53c
Tara <144671692+TaraG17@users.noreply.github.com>
2024-01-21 13:35:06 +0100

    Update Module_1.py

--- Module_1.py
+++ Module_1.py
@@ -8,23 +8,22 @@ time = datetimeNow.strftime("%H:%M:%S")
 print(date, time)
 
 #---User input for message---
-bericht = input('Type hier uw bericht: ')
-if(len(bericht)>= 140):
-    print('Uw bericht is te lang')
-else:
-    print(bericht)
+while True:
+    bericht = input('Type hier uw bericht: ')
+    if len(bericht) >= 140: # als het bericht langer is dan 140 karakters, moet de user opnieuw een bericht intypen
+        print('Uw bericht is te lang. Probeer opnieuw.')
+    else:
+        break # zodra de user een bericht intypt wat kleiner is dan 140 karakters, wordt de loop verbroken
 
-print('Laat dit veld leeg om anoniem te blijven')
+print('Je kan dit volgende veld leeg laten om anoniem te blijven')
 naam = input('Type hier uw naam: ')
-geenNaam = 'anoniem'
+geenNaam = 'Anoniem'
 if naam == '':
     print(geenNaam)
-else:
-    print(naam)
 
 #---Randomizing station per message---
 station = ['Utrecht', 'Amsterdam', 'Rotterdam']
-randomstation = random.choice(station)
+randomstation = random.choice(station) # kiest een random station uit
 
 #---Reading file---
 file = open("testmodule1.csv")  # opent de file
@@ -39,3 +38,4 @@ resultaat = date + "|" + time + "|" + randomstation + " | " + str(naam) + " " + 
 nieuw = open("testmodule1.csv", "a")  # Wijzigt de file
 nieuw.write(resultaat)
 nieuw.close()
+
